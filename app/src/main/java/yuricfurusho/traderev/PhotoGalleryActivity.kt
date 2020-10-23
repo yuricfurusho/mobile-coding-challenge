633cf64ed2c1a88067b5253c591f7980aa804851
yurifurusho <yuri>
2020-10-23 15:26:31 -0400

    step-3.1: setup of architecture: renames activity to improve readability

--- app/src/main/java/yuricfurusho/traderev/PhotoGalleryActivity.kt
+++ app/src/main/java/yuricfurusho/traderev/PhotoGalleryActivity.kt
@@ -7,19 +7,19 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_photo_gallery.*
 import yuricfurusho.traderev.photos.UnsplashService
 
 private const val SPAN_COUNT = 2
 
-class MainActivity : AppCompatActivity() {
+class PhotoGalleryActivity : AppCompatActivity() {
 
     private val compositeDisposable = CompositeDisposable() //TODO: move away from ui into viewModel or repository
     private val unsplashService = UnsplashService.create() //TODO: remove later: for testing only
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_photo_gallery)
 
         recycler_photos.layoutManager = StaggeredGridLayoutManager(
                 SPAN_COUNT,
