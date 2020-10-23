42bd8cfc89b54cc932dd1646a2081f2dcbab6e79
yurifurusho <yuri>
2020-10-23 12:17:25 -0400

    step-1.1: setups the UI: setups recyclerview for the list of photos

--- app/src/main/java/yuricfurusho/traderev/MainActivity.kt
+++ app/src/main/java/yuricfurusho/traderev/MainActivity.kt
@@ -1,11 +1,21 @@
 package yuricfurusho.traderev
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val SPAN_COUNT = 2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_photos.layoutManager = StaggeredGridLayoutManager(
+            SPAN_COUNT,
+            StaggeredGridLayoutManager.VERTICAL
+        )
+//        recycler_photos.adapter = PhotosAdapter()
     }
 }
