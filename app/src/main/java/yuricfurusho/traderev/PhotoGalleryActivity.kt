038b850722a45d79832c1f0e8221d3ee96f0cf99
yurifurusho <yuri>
2020-10-23 21:41:42 -0400

    step-4.1: Setups Paging: Simple implementation for loading next page

--- app/src/main/java/yuricfurusho/traderev/PhotoGalleryActivity.kt
+++ app/src/main/java/yuricfurusho/traderev/PhotoGalleryActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
 import kotlinx.android.synthetic.main.activity_photo_gallery.*
 import yuricfurusho.traderev.photos.PhotoGalleryViewModel
 import javax.inject.Inject
@@ -17,26 +20,44 @@ class PhotoGalleryActivity : AppCompatActivity() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel by viewModels<PhotoGalleryViewModel> { viewModelFactory }
 
+    private lateinit var photoAdapter: PhotoAdapter
+    private lateinit var staggeredGridLayoutManager: StaggeredGridLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as TradeRevApp).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_gallery)
 
-        recycler_photos.layoutManager = StaggeredGridLayoutManager(
-            SPAN_COUNT,
-            StaggeredGridLayoutManager.VERTICAL
-        )
-        recycler_photos.adapter = PhotoAdapter()
+        photoAdapter = PhotoAdapter()
+        staggeredGridLayoutManager = StaggeredGridLayoutManager(SPAN_COUNT, VERTICAL)
+        staggeredGridLayoutManager.gapStrategy = GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+        recycler_photos.apply {
+            adapter = photoAdapter
+            layoutManager = staggeredGridLayoutManager
+            addOnScrollListener(onScrollListener())
+        }
 
         viewModel.onCreate()
 
         viewModel.state.observe(this, {
             when (it) {
-                is PhotoGalleryViewModel.State.Success ->
-                    (recycler_photos.adapter as PhotoAdapter).setList(it.photoList)
+                is PhotoGalleryViewModel.State.Success -> photoAdapter.setList(it.photoList)
                 is PhotoGalleryViewModel.State.Error -> TODO()
             }
         })
 
+    }
+
+    private fun onScrollListener(): RecyclerView.OnScrollListener {
+        return object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val lastVisibleItemsIndexes =
+                    staggeredGridLayoutManager.findLastVisibleItemPositions(null)
+                val lastLoadedItemIndex = (photoAdapter.itemCount - 1).coerceAtLeast(0)
+                if (lastVisibleItemsIndexes.contains(lastLoadedItemIndex)) {
+                    viewModel.loadNextPage()
+                }
+            }
+        }
     }
 }
