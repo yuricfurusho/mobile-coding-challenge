45369e016c9c2728c1acebaf05283432a5e59ea9
yurifurusho <yuri>
2020-10-23 20:28:38 -0400

    step-3.3: setup of architecture: Moves existing code to proper layers

--- app/src/main/java/yuricfurusho/traderev/PhotoGalleryActivity.kt
+++ app/src/main/java/yuricfurusho/traderev/PhotoGalleryActivity.kt
@@ -1,17 +1,12 @@
 package yuricfurusho.traderev
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_photo_gallery.*
 import yuricfurusho.traderev.photos.PhotoGalleryViewModel
-import yuricfurusho.traderev.photos.UnsplashService
 import javax.inject.Inject
 
 private const val SPAN_COUNT = 2
@@ -21,9 +16,6 @@ class PhotoGalleryActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel by viewModels<PhotoGalleryViewModel> { viewModelFactory }
-
-    private val compositeDisposable = CompositeDisposable() //TODO: move away from ui into viewModel or repository
-    private val unsplashService = UnsplashService.create() //TODO: remove later: for testing only
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as TradeRevApp).appComponent.inject(this)
@@ -36,24 +28,15 @@ class PhotoGalleryActivity : AppCompatActivity() {
         )
         recycler_photos.adapter = PhotoAdapter()
 
+        viewModel.onCreate()
 
-        unsplashService.getPhotos(1, 10) //TODO: move away from ui into viewModel or repository
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ photoList ->
-                    (recycler_photos.adapter as PhotoAdapter).setList(
-                            photoList.map { it.thumb }
-                    )
-                }, {
-                    Log.e("MainActivity", it.localizedMessage ?: "")
-                })
-                .also {
-                    compositeDisposable.add(it)
-                }
-    }
+        viewModel.state.observe(this, {
+            when (it) {
+                is PhotoGalleryViewModel.State.Success ->
+                    (recycler_photos.adapter as PhotoAdapter).setList(it.photoList)
+                is PhotoGalleryViewModel.State.Error -> TODO()
+            }
+        })
 
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.clear() //TODO: move away from ui into viewModel or repository
     }
 }
--- app/src/main/java/yuricfurusho/traderev/photos/PhotoRepository.kt
+++ app/src/main/java/yuricfurusho/traderev/photos/PhotoRepository.kt
@@ -1,7 +1,12 @@
 package yuricfurusho.traderev.photos
 
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class PhotoRepository @Inject constructor(
-    private val unsplashApi: UnsplashService
-)
+    private val unsplashService: UnsplashService
+) {
+    fun getFirstPage() = unsplashService.getPhotos(1, 10)
+}
+
